37c31ac4290342fd764142ca7687364db0286c96
Pedro Previatti <pvlpreviatti@gmail.com>
2024-03-04 10:43:16 -0300

    go mod and comments

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Pedro-Previatti/go-apps
+
+go 1.22.0
--- main.go
+++ main.go
@@ -7,13 +7,18 @@ import (
 
 func main() {
 
+	// when the application starts the page response should be 'Hello, world!'
+	// and the console should print 13 as the number of bytes got from Fprintf
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, world")
+		n, err := fmt.Fprintf(w, "Hello, world!")
+		// error handling will print the error on console
+		// not the best handling but simple for this application
 		if err != nil {
 			fmt.Print(err)
 		}
 		fmt.Println(fmt.Sprintf("Numbers of bytes written: %d", n))
 	})
 
+	// listens to port 8080
 	_ = http.ListenAndServe(":8080", nil)
 }
