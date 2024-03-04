a2f62b980a797ee856668d3c05aa6856b1232f0a
Pedro Previatti <pvlpreviatti@gmail.com>
2024-03-04 15:18:51 -0300

    handler funcs

--- main.go
+++ main.go
@@ -5,20 +5,33 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("The about returned value was %d", sum))
+}
+
+// addValues adds two values
+// if it starts with a lower case it means the function is not going to leave
+// the scope of this file, otherwise it can be called from anywhere
+func addValues(x, y int) int {
+	return x + y
+}
+
+// main is the main application function
 func main() {
 
-	// when the application starts the page response should be 'Hello, world!'
-	// and the console should print 13 as the number of bytes got from Fprintf
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, world!")
-		// error handling will print the error on console
-		// not the best handling but simple for this application
-		if err != nil {
-			fmt.Print(err)
-		}
-		fmt.Println(fmt.Sprintf("Numbers of bytes written: %d", n))
-	})
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
+	fmt.Println(fmt.Sprintf("application started \nrunning on port%s", portNumber))
 	// listens to port 8080
-	_ = http.ListenAndServe(":8080", nil)
+	_ = http.ListenAndServe(portNumber, nil)
 }
